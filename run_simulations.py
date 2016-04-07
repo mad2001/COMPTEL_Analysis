9ba9656aaf3582f155da24932a7b52eb742c7c5a
mad2001 <mad2001@wildcats.unh.edu>
2016-04-07 01:04:00 -0400

    worked on run_simulations module

--- run_simulations.py
+++ run_simulations.py
@@ -5,22 +5,24 @@ Created on Tue Apr  5 20:50:52 2016
 @author: Morgan
 """
 
-source_file = SOME_PATH
+import os
+import subprocess
+
+source_file = 'SOME_PATH'
 
 for i, energy in enumerate(range(1, 100, 5)):
 
     # edit source file
-    f1 = open('souce_file', 'r')
-    f2 = open('souce_file', 'w')
-    for line in f1:
-        f2.write(line)
-    f1.close()
-    f2.close()
+    with open(souce_file, 'r') as in_file, open(souce_file, 'w') as out_file:
+        for line in in_file:
+            out_file.write(line)
+
 
     # change directory
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
+    os.chdir(path)
 
     # run cosima
-    run("cosima" "filename")
+    subprocess.run("cosima" "filename")
